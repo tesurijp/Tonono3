0ebde5d69fd658eb9686eb36bbf36b08f3242ff1
tesuri <tesuri@gmail.com>
2026-08-11 16:32:15 +0900

    Word対応

--- src/Tonono3/Win32/OutputManager.cs
+++ src/Tonono3/Win32/OutputManager.cs
@@ -12,6 +12,15 @@ public static class OutputManager
     public static void SendString(string text)
     {
         var inputs = new List<NativeMethods.INPUT>();
+
+        var ctrlPressed = (NativeMethods.GetKeyState(VK_CONTROL) & 0x8000) != 0;
+        if (ctrlPressed)
+        {
+            var ctrlUp = new NativeMethods.INPUT { type = INPUT_KEYBOARD };
+            ctrlUp.U.ki = new() { wVk = (ushort)VK_CONTROL, wScan = 0, dwFlags = KEYEVENTF_KEYUP, time = 0, dwExtraInfo = IntPtr.Zero };
+            inputs.Add(ctrlUp);
+        }
+
         foreach (var c in text)
         {
             var down = new NativeMethods.INPUT { type = INPUT_KEYBOARD };
@@ -23,6 +32,14 @@ public static class OutputManager
             inputs.Add(down);
             inputs.Add(up);
         }
+
+        if (ctrlPressed)
+        {
+            var ctrlDown = new NativeMethods.INPUT { type = INPUT_KEYBOARD };
+            ctrlDown.U.ki = new() { wVk = (ushort)VK_CONTROL, wScan = 0, dwFlags = 0, time = 0, dwExtraInfo = IntPtr.Zero };
+            inputs.Add(ctrlDown);
+        }
+
         NativeMethods.SendInput((uint)inputs.Count, [.. inputs], Marshal.SizeOf<NativeMethods.INPUT>());
     }
 }
