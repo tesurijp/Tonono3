d553bc55810196592c2c6f8fa749600cafc91a7e
tesuri <tesuri@gmail.com>
2026-08-25 21:08:46 +0900

    情報表示画面の冗長な部分を修正

--- src/Tonono3/UI/InfoViewModel.cs
+++ src/Tonono3/UI/InfoViewModel.cs
@@ -1,48 +1,13 @@
 using System.Collections.Generic;
-using System.Linq;
 using Tonono3.SKKEngine;
 
 namespace Tonono3.UI;
 
-public sealed class InfoStringPairRow
+public record class InfoViewModel(AppConfig Config, string ConfigPath)
 {
-    public required string Key { get; init; }
-    public required string Value { get; init; }
-}
-
-public sealed class InfoCharStringPairRow
-{
-    public required char Key { get; init; }
-    public required string Value { get; init; }
-}
-
-public class InfoViewModel
-{
-    public InfoViewModel(AppConfig config, string configPath)
-    {
-        ConfigPath = configPath;
-        RomajiEntries = config.RomajiEntries
-            .Select(kv => new InfoStringPairRow { Key = kv.Key, Value = kv.Value })
-            .ToArray();
-        ZenkakuEntries = config.ZenkakuEntries
-            .Select(kv => new InfoCharStringPairRow { Key = kv.Key, Value = kv.Value })
-            .ToArray();
-        MoraModifierEntries = config.MoraEntries
-            .Select(kv => new InfoStringPairRow { Key = kv.Key, Value = kv.Value })
-            .ToArray();
-        MoraAutoCompleteEntries = config.MoraCompleteEntries
-            .Select(kv => new InfoStringPairRow { Key = kv.Key, Value = kv.Value })
-            .ToArray();
-        DictionaryPaths = config.DictionaryPaths;
-        ViCompatibleApps = config.ViCompatibleApps;
-    }
-
+    public IEnumerable<KeyValuePair<string, string>> Romaji => Config.RomajiEntries;
+    public IEnumerable<KeyValuePair<string, string>> Mora => Config.MoraEntries;
+    public IEnumerable<KeyValuePair<string, string>> MoraComplete => Config.MoraCompleteEntries;
+    public IEnumerable<KeyValuePair<char, string>> Zenkaku => Config.ZenkakuEntries;
     public static string VersionInfo => $"Version {BuildInfo.Version} ( SkkEngine version {SkkEngine.BuildInfo.Version}; Interop version {Interop.BuildInfo.Version} )";
-    public string ConfigPath { get; }
-    public IEnumerable<InfoStringPairRow> RomajiEntries { get; }
-    public IEnumerable<InfoCharStringPairRow> ZenkakuEntries { get; }
-    public IEnumerable<InfoStringPairRow> MoraModifierEntries { get; }
-    public IEnumerable<InfoStringPairRow> MoraAutoCompleteEntries { get; }
-    public IEnumerable<string> DictionaryPaths { get; }
-    public IEnumerable<string> ViCompatibleApps { get; }
 }
